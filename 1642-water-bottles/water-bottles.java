f01ecd2385811dd8f1bb12071c99bf567cbddf22
SAI BHARGAV EDIGA <sai.bhargav.ediga@gmail.com>
2026-09-11 15:26:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.3 MB (19.12%) - LeetSync

--- 1642-water-bottles/water-bottles.java
+++ 1642-water-bottles/water-bottles.java
@@ -1,12 +1,5 @@
 class Solution {
-    public int numWaterBottles(int numBottles, int numExchange) {
-        int totalBottles = numBottles;
-
-        while (numBottles >= numExchange) {
-            totalBottles += numBottles / numExchange;
-            numBottles = (numBottles / numExchange) + (numBottles % numExchange);
-        }
-
-        return totalBottles;
+    public int numWaterBottles(int b, int n) {
+        return b + (b - 1) / (n - 1);
     }
 }
